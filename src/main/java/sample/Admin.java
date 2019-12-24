f933c1d58e0fda204c1620b29d6820cc894e5f20
Pawel <kuriatapaw@gmail.com>
2019-12-24 12:02:22 +0100

    lol?

---
+++ src/main/java/sample/Admin.java
@@ -0,0 +1,50 @@
+package sample;
+
+import javax.persistence.*;
+
+@Entity (name = "admin")
+public class Admin {
+    @Id
+    @Column (name = "admin_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int adminId;
+    @Column (name="login")
+    private String adminLogin;
+    @Column (name = "adres_email")
+    private String adminEmail;
+
+    public String getAdminLogin() {
+        return adminLogin;
+    }
+
+    public void setAdminLogin(String adminLogin) {
+        this.adminLogin = adminLogin;
+    }
+
+    @Column (name = "haslo")
+    private String adminPassword;
+
+    public String getAdminPassword() {
+        return adminPassword;
+    }
+
+    public void setAdminPassword(String adminPassword) {
+        this.adminPassword = adminPassword;
+    }
+
+    public String getAdminEmail() {
+        return adminEmail;
+    }
+
+    public void setAdminEmail(String adminEmail) {
+        this.adminEmail = adminEmail;
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+}
