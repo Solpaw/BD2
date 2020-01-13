1950a71df4ea00179eb82a12fb43d9dff89cf68e
Pawel <kuriatapaw@gmail.com>
2020-01-13 15:28:50 +0100

    database

--- BD2.sql
+++ BD2.sql
@@ -11,7 +11,8 @@ DROP TABLE biegaczLog CASCADE;
 CREATE TABLE Lokalizacja(
     lokalizacja_id SERIAL Primary KEY,
     miejscowosc VARCHAR(30) NOT NULL,
-    ulica VARCHAR(30) NOT NULL
+    ulica VARCHAR(30) NOT NULL,
+    unique (miejscowosc,ulica)
 );
 
 CREATE TABLE Biegacz(
@@ -26,7 +27,8 @@ CREATE TABLE Biegacz(
 
 CREATE TABLE Cena(
     id SERIAL PRIMARY KEY,
-    cena INTEGER NOT NULL
+    cena INTEGER NOT NULL,
+    UNIQUE (cena)
 );
 
 CREATE TABLE Trasa(
@@ -34,6 +36,7 @@ CREATE TABLE Trasa(
     dlugosc INTEGER NOT NULL,
     ilosc_przeszkod INTEGER,
     cena_id INTEGER not null,
+    UNIQUE (dlugosc,ilosc_przeszkod,cena_id),
     FOREIGN KEY (cena_id) REFERENCES Cena(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
@@ -68,6 +71,7 @@ CREATE TABLE  Wyniki(
     biegacz_id INTEGER not null,
     miejsce INTEGER,
     czas NUMERIC(6,2),
+    unique (bieg_id,biegacz_id),
     FOREIGN KEY (biegacz_id) REFERENCES Biegacz(biegacz_id) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (bieg_id) REFERENCES Bieg(bieg_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -192,47 +196,68 @@ CREATE OR REPLACE FUNCTION usunAdmina(nr integer) returns void as $$
 LANGUAGE PLPGSQL;
 
 
-select dodajadmina('Admin_1','admin1@gmail.com','1234');
-select dodajAdmina('Admin_2', 'admin2@gmail.com','4321');
-select dodajAdmina('Admin_3', 'admin3@gmail.com','1111');
+select dodajadmina('Solpaw','sio@gmail.com','1234');
+
+
 
 select dodajLokalizajce('Augustowska','Lubin');
 select dodajLokalizajce('Szkolna','Wrocław');
-select dodajLokalizajce('Teczowa','Swidnica');
-select dodajLokalizajce('Kochanoskiego','Jelenia Góra');
-select dodajLokalizajce('Obozowa','Siechnice');
+select dodajLokalizajce('Teczowa','Wrocław');
+select dodajLokalizajce('Kochanowskiego','Wrocław');
+select dodajLokalizajce('Obozowa','Lubin');
 
 select dodajcene(10);
 select dodajcene(20);
 select dodajcene(30);
-select dodajcene(40);
 
-select dodajTrase(10,1,5);
-select dodajTrase(10,1,10);
-select dodajTrase(15,4,6);
-select dodajTrase(30,3,15);
+select dodajTrase(100,1,5);
+select dodajTrase(100,1,10);
+select dodajTrase(1000,3,20);
+select dodajTrase(500,2,15);
 
-select dodajBiegacza('Piotr', 'Wijak','peter234@gmail.com','1998-09-21','M','1234');
+select dodajBiegacza('Piotr', 'Strachoń','dio@gmail.com','1998-09-21','M','1234');
 select dodajBiegacza('Karol','Tomaszewski', 'mister8@gmail.com','1968-06-06','XXL','2345');
-select dodajBiegacza('James','Poniatowski', 'dio@gmail.com','1966-06-06','S','3456');
+select dodajBiegacza('Jan','Poniatowski', 'tyu@gmail.com','1966-06-06','S','3456');
 select dodajBiegacza('Maciej','Godlewski', 'biegacz@gmail.com','1999-11-12','L','4311');
-select dodajBiegacza('Krzysztof','Gonciarz', 'odpada4@gmail.com','2000-04-01','XS','8765');
+select dodajBiegacza('Maja','Kowalska', 'kowalska@gmail.com','1988-04-01','S','865432');
+select dodajBiegacza('Marta','Dziurman', 'dziurman@gmail.com','1999-08-15','S','12357');
+select dodajBiegacza('Kamila','Rakowska', 'radowska@gmail.com','1993-02-08','S','6423');
+select dodajBiegacza('Luiza','Cichoń', 'cich@gmail.com','1996-09-24','M','098776');
+select dodajBiegacza('Emilia','Stachowiak', 'stach@gmail.com','1998-06-16','S','2342');
+select dodajBiegacza('Szymon','Piechowski', 'piechu@gmail.com','1989-03-28','M','54647');
 
-select dodajBieg(1,'2019-02-01',1);
-select dodajBieg(1,'2019-03-01',1);
-select dodajBieg(2,'2019-04-01',2);
-select dodajBieg(3,'2019-05-01',3);
-select dodajBieg(4,'2019-06-01',4);
-select dodajBieg(2,'2020-06-01',3);
-select dodajBieg(2,'2020-06-05',4);
-select dodajBieg(2,'2020-08-01',1);
+select dodajBieg(1,'2019-04-01',1);
+select dodajBieg(4,'2019-06-01',3);
+select dodajBieg(3,'2020-02-01',4);
+select dodajBieg(2,'2020-02-05',5);
+
+select dodajZapis(1,1);
+select dodajZapis(2,1);
+select dodajZapis(3,1);
+select dodajZapis(4,1);
+select dodajZapis(5,1);
+select dodajZapis(6,1);
+
+select dodajZapis(5,2);
+select dodajZapis(4,2);
+select dodajZapis(8,2);
+select dodajZapis(9,2);
+
+select dodajZapis(6,3);
+select dodajZapis(2,3);
+select dodajZapis(7,3);
+select dodajZapis(10,3);
+select dodajZapis(9,4);
+select dodajZapis(8,4);
 
 select dodajWynik(1, 1,1, 18.54);
-select dodajWynik(2, 1,2, 19.32);
-select dodajWynik(3, 1,2, 22.09);
-select dodajWynik(4, 2,2, 25.27);
-select dodajWynik(5, 2,1, 18.12);
+select dodajWynik(2, 1,2, 19.74);
+select dodajWynik(3, 1,3, 19.84);
+select dodajWynik(4, 1,4, 20.21);
+select dodajWynik(5, 1,5, 21.54);
+select dodajWynik(6, 1,6, 22.78);
 
-select dodajZapis(1,2);
-select dodajZapis(1,3);
-select dodajZapis(2,5);
+select dodajWynik(5, 2,1, 30.56);
+select dodajWynik(4, 2,2, 32.76);
+select dodajWynik(9, 2,4, 35.98);
+select dodajWynik(8, 2,3, 34.06);
